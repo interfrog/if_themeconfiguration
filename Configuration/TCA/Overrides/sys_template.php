95b4bcde09067405c4f12ee5f4ba8f65b544588b
elge <elge@iMac-EG.local>
2019-06-27 14:48:38 +0200

    [BUGFIX] allow colorconfig on pages

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,8 +2,3 @@
 
 $tsTemplateName = 'if_themeconfiguration - Theme Configuration';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('if_themeconfiguration','Configuration/TypoScript',$tsTemplateName);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_themeconfiguration");
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_color");
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_font");
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_colorsheme");
--- ext_tables.php
+++ ext_tables.php
@@ -2,3 +2,8 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_themeconfiguration");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_color");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_font");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_ifthemeconfiguration_domain_model_colorsheme");
